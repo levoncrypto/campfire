4b001669c99df9afb2d9d376ca93ec1995d77fa1
sneurlax <sneurlax@gmail.com>
2026-01-07 16:11:18 -0600

    fix: use rust 1.89.0 for xelis
    it works for frostdart etc as well

--- scripts/rust_version.sh
+++ scripts/rust_version.sh
@@ -3,9 +3,9 @@
 
 set_rust_to_everything_else() {
   if rustup toolchain list | grep -q "1.85.1"; then
-    rustup default 1.85.1
+    rustup default 1.89.0
   else
-    echo "Rust version 1.85.1 is not installed. Please install it using 'rustup install 1.85.1'." >&2
+    echo "Rust version 1.89.0 is not installed. Please install it using 'rustup install 1.89.0'." >&2
     exit 1
   fi
 }

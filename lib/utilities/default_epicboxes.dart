87f371673bd416aa0d0a6b1e8e42bcd4aea192ba
sneurlax <sneurlax@gmail.com>
2026-02-03 12:28:17 -0600

    feat: use epicbox.epiccash.com as default epicbox server
    and format the rest

--- lib/utilities/default_epicboxes.dart
+++ lib/utilities/default_epicboxes.dart
@@ -16,38 +16,49 @@ abstract class DefaultEpicBoxes {
   static List<EpicBoxServerModel> get all => [americas, asia, europe];
   static List<String> get defaultIds => ['americas', 'asia', 'europe'];
 
+  static EpicBoxServerModel get epiccashCom => EpicBoxServerModel(
+    host: 'epicbox.epiccash.com',
+    port: 443,
+    name: 'Official',
+    id: 'epiccashCom',
+    useSSL: true,
+    enabled: true,
+    isFailover: true,
+    isDown: false,
+  );
+
   static EpicBoxServerModel get americas => EpicBoxServerModel(
-        host: 'epicbox.stackwallet.com',
-        port: 443,
-        name: 'Americas',
-        id: 'americas',
-        useSSL: true,
-        enabled: true,
-        isFailover: true,
-        isDown: false,
-      );
+    host: 'epicbox.stackwallet.com',
+    port: 443,
+    name: 'Stack Wallet',
+    id: 'americas',
+    useSSL: true,
+    enabled: true,
+    isFailover: true,
+    isDown: false,
+  );
 
   static EpicBoxServerModel get asia => EpicBoxServerModel(
-        host: 'epicbox.hyperbig.com',
-        port: 443,
-        name: 'Asia',
-        id: 'asia',
-        useSSL: true,
-        enabled: true,
-        isFailover: true,
-        isDown: false,
-      );
+    host: 'epicbox.hyperbig.com',
+    port: 443,
+    name: 'Asia',
+    id: 'asia',
+    useSSL: true,
+    enabled: true,
+    isFailover: true,
+    isDown: false,
+  );
 
   static EpicBoxServerModel get europe => EpicBoxServerModel(
-        host: 'epicbox.fastepic.eu',
-        port: 443,
-        name: 'Europe',
-        id: 'europe',
-        useSSL: true,
-        enabled: true,
-        isFailover: true,
-        isDown: false,
-      );
+    host: 'epicbox.fastepic.eu',
+    port: 443,
+    name: 'Europe',
+    id: 'europe',
+    useSSL: true,
+    enabled: true,
+    isFailover: true,
+    isDown: false,
+  );
 
-  static final defaultEpicBoxServer = americas;
+  static final defaultEpicBoxServer = epiccashCom;
 }
